0abc3543f2847a8ef9242402a6a0dfca4ab9b4b4
LigongChen <jiashisoft@qq.com>
2017-05-20 23:40:08 +0800

    Add files via upload

---
+++ about.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Jiashisoft Community Open Source Project";
+	cout<<"About '.e' format file please see in www.dywt.com.cn"
+	return 0;	
+ }; 
